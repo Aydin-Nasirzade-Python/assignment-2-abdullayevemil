3a87e224925915de8b96bb429f9fdee4c1da5275
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-04-07 19:27:44 +0400

    Update Task_7.py

--- Task_7.py
+++ Task_7.py
@@ -5,7 +5,7 @@ def main():
 
   y = float(input("Enter y: "))
   
-  if (y >= (x - 2) ** 2 - 3 and x <= abs(y)) or (y >= (x - 2) ** 2 - 3 and y >= 0 and x >= abs(y)):
+  if (y >= (x - 2) ** 2 - 3 and x <= abs(y) and y <= 0) or (y >= (x - 2) ** 2 - 3 and y >= 0 and x >= abs(y)):
       print("The point is in the shaded area")
   else:
       print("The point is not in the shaded area")
