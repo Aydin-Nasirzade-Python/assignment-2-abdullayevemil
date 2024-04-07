51fdab8b3882503e97395bfb533794daa5ed77d6
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-04-07 17:31:54 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,9 +1,22 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  length = float(input("Enter the wavelength in nm: "))
+
+  if length <= 750 and length >= 620:
+      print("The relevant color is Red")
+  elif length >= 590:
+      print("The relevant color is Orange")
+  elif length >= 570:
+      print("The relevant color is Yellow")
+  elif length >= 495:
+      print("The relevant color is Green")
+  elif length >= 450:
+      print("The relevant color is Blue")
+  elif length >= 380:
+      print("The relevant color is Violet")
+  else:
+      print("Invalid input!")
   pass
 
 if __name__ == "__main__":
